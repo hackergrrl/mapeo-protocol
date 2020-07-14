c0cd1d8845d429d4989d80dca9ff069db926e947
Kira Oakley <kira@eight45.net>
2020-07-14 13:52:19 -0700

    add the Heartbeat RPC and tests

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 const muxrpc = require('muxrpc')
-const pull = require('pull-stream')
+const timer = require('timeout-refresh')
 
 const PLAINTEXT_PROTOCOL_VERSION = '1.0.0'
 const SECURE_PROTOCOL_VERSION    = '1.0.0'
 
+function noop () {}
+
 const rpcManifest = {
   GetInfo: 'async',
+  Heartbeat: 'async',
   CreateSecureChannel: 'duplex'
 }
 
 class MapeoProtocol {
-  constructor () {
+  constructor (opts) {
+    opts = opts || {}
+
     const api = {
       GetInfo: this.rpcGetPeerInfo.bind(this),
+      Heartbeat: this.rpcHeartbeat.bind(this),
       CreateSecureChannel: this.rpcCreateSecureChannel.bind(this)
     }
     this.rpc = muxrpc(rpcManifest, rpcManifest)(api)
+    this.rpcStream = null
+
+    this.timeoutMs = opts.timeout || 20000
+
+    // Timers
+    this.timeout = null
+    this.heartbeat = null
   }
 
   rpcGetPeerInfo (cb) {
@@ -25,15 +38,64 @@ class MapeoProtocol {
     })
   }
 
-  // TODO: implementation
-  rpcCreateSecureChannel () {
-    return null
+  rpcHeartbeat (cb) {
+    this.timeout.refresh()
+    this.heartbeat.refresh()
+    cb()
   }
 
-  createStream () {
-    return this.rpc.createStream()
+  rpcCreateSecureChannel () {
+    throw new Error('not implemented')
+  }
+
+  createStream (cb) {
+    if (this.rpcStream) return false
+    cb = cb || noop
+
+    this.rpcStream = this.rpc.createStream(err => {
+      this.onRpcClose(err)
+      cb(err)
+    })
+
+    this.timeout = timer(this.timeoutMs, this.onTimeout, this)
+    this.heartbeat = timer(Math.floor(this.timeoutMs/2), this.onHeartbeat, this)
+
+    return this.rpcStream
+  }
+
+  onRpcClose (err) {
+    this.timeout.destroy()
+    this.heartbeat.destroy()
+    this.rpcStream = null
+    this.timeout = null
+    this.heartbeat = null
+  }
+
+  onTimeout () {
+    this.close(new Error('remote timeout'))
+  }
+
+  onHeartbeat () {
+    this.heartbeat.destroy()
+    this.heartbeat = timer(Math.floor(this.timeoutMs/2), this.onHeartbeat, this)
+
+    this.rpc.Heartbeat((err) => {
+      if (err) return
+      this.timeout.refresh()
+      this.heartbeat.refresh()
+    })
+  }
+
+  close (err, cb) {
+    cb = cb || noop
+    if (this.rpcStream) {
+      this.timeout.destroy()
+      this.heartbeat.destroy()
+      this.rpc.close(err, cb.bind(null, err))
+    } else {
+      process.nextTick(cb)
+    }
   }
 }
 
 module.exports = MapeoProtocol
-
--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 const test = require('tape')
 const pull = require('pull-stream')
+const muxrpc = require('muxrpc')
 const Protocol = require('..')
 
 test('can create & get duplex stream', function (t) {
@@ -32,3 +33,82 @@ test('rpc: GetPeerInfo', function (t) {
   })
 })
 
+test('rpc: Heartbeat', function (t) {
+  t.plan(2)
+
+  const proto1 = new Protocol()
+  const proto2 = new Protocol()
+  const stream1 = proto1.createStream()
+  const stream2 = proto2.createStream()
+
+  pull(stream1, stream2, stream1)
+
+  proto1.rpcHeartbeat(err => {
+    t.error(err, 'stream1 heartbeat ok')
+  })
+
+  proto2.rpcHeartbeat(err => {
+    t.error(err, 'stream2 heartbeat ok')
+  })
+})
+
+test('heartbeats keep connection alive', function (t) {
+  t.plan(1)
+
+  const opts = { timeout: 100 }
+
+  const proto1 = new Protocol(opts)
+  const proto2 = new Protocol(opts)
+  const stream1 = proto1.createStream(prematureEnd)
+  const stream2 = proto2.createStream(prematureEnd)
+
+  pull(
+    stream1,
+    stream2,
+    stream1
+  )
+
+  setTimeout(() => {
+    t.pass('connection kept alive ok')
+  }, 400)
+
+  function prematureEnd (err) {
+    t.fail('protocol should not terminate')
+  }
+})
+
+test('protocol times out without heartbeat responses', function (t) {
+  t.plan(2)
+
+  const opts = { timeout: 200 }
+
+  const proto1 = new Protocol(opts)
+  const stream1 = proto1.createStream(onEnd)
+  const stream2 = createFakeApiIgnoreHeartbeats().createStream()
+
+  let ended = false
+
+  pull(stream1, stream2, stream1)
+
+  const id = setTimeout(() => {
+    t.fail('timed out without ending properly')
+  }, 400)
+
+  function onEnd (err) {
+    t.ok(err, 'ended with error')
+    t.ok(/remote timeout/.test(err.message), 'error message ok')
+    clearTimeout(id)
+  }
+})
+
+function createFakeApiIgnoreHeartbeats () {
+  const rpcManifest = {
+    Heartbeat: 'async'
+  }
+  const api = {
+    Heartbeat: function (cb) {
+      // do nothing
+    }
+  }
+  return muxrpc(rpcManifest, rpcManifest)(api)
+}
